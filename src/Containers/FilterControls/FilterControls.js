c752daf2f88338772d10abf37d584bb31592b5eb
Adam Niedzwiecki <adamn8142@gmail.com>
2019-05-30 22:09:02 -0600

    Fetch strain data

--- src/Containers/FilterControls/FilterControls.js
+++ src/Containers/FilterControls/FilterControls.js
@@ -1,54 +1,83 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { addSativa } from '../../Actions'
+import { addSativa, addIndica, addHybrid } from '../../Actions'
 
 class FilterControls extends Component {
 
   constructor(){
     super();
     this.state = {
-      selectedFilter : ''
+      selectedFilter: ''
     }
   }
 
 
-  fetchStrain =  async (strain) => {
-    const url = `http://strainapi.evanbusse.com/AqtPtuS/strains/search/race/${strain}`
+  // fetchStrain =  async (strain) => {
+  //   const url = `http://strainapi.evanbusse.com/AqtPtuS/strains/search/race/${strain}`
+  //   const response = await fetch(url)
+  //   const results = await response.json()
+  //   console.log(results)
+  //     if(this.state.selectedFilter === 'sativa'){
+  //       await this.props.addSativa(results)
+  //     } else if (this.state.selectedFilter === 'hybrid'){
+  //       await this.props.addHybrid(results)
+  //     } else if (this.props.selectedFilter === 'indica') {
+  //       await this.props.addIndica(results)
+  //     }
+  // }
+
+  fetchSativa=  async () => {
+    const url = 'http://strainapi.evanbusse.com/AqtPtuS/strains/search/race/sativa'
     const response = await fetch(url)
     const results = await response.json()
-      if(this.state.selectedFilter === 'Sativa'){
-        await this.props.addSativa(results)
-      } else if (this.state.selectedFilter === 'Hybrid'){
-        await this.props.addHybrid(results)
-      } else if (this.props.selectedFilter === 'Indica') {
-        await this.props.addIndica(results)
-      }
+    console.log(results)
+      this.props.addSativa(results)
   }
+
+  fetchIndica=  async () => {
+    const url = 'http://strainapi.evanbusse.com/AqtPtuS/strains/search/race/indica'
+    const response = await fetch(url)
+    const results = await response.json()
+    console.log(results)
+      this.props.addIndica(results)
+  }
+
+  fetchHybrid=  async () => {
+    const url = 'http://strainapi.evanbusse.com/AqtPtuS/strains/search/race/hybrid'
+    const response = await fetch(url)
+    const results = await response.json()
+    console.log(results)
+      this.props.addHybrid(results)
+  }
+
 
   setFilter = (event) => {
+    event.preventDefault();
     this.setState({
       selectedFilter: event.target.value
-    }, this.fetchStrain(this.state.selectedFilter))
+    })
   }
 
-  //SHOULD I BE BREAKING EACH FETCH OUT INDIVIDUALLY INSTEAD OF JUST A FETCH STRAIN 
 
-  
   render() {
     return (
       <div>
         <button
-          value="Sativa"
-          onClick={this.setFilter}>SATIVA
+          value="sativa"
+          onClick={this.fetchSativa}>SATIVA
         </button>
         <button
-          value="Hybrid"
-          onClick={this.setFilter}>HYBRID
+          value="hybrid"
+          onClick={this.fetchtHybrid}>HYBRID
         </button>
         <button
-          value="Indica"
-          onClick={this.setFilter}>INDICA
+          value="indica"
+          onClick={this.fetchIndica}>INDICA
         </button>
+        <button
+        onClick={this.fetchIndica}
+        
+        >CLICK ME </button>
       </div>
     )
   }
@@ -61,8 +90,10 @@ strain: state.strain
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  addSativa: (sativa) => dispatch(addSativa(sativa))
-  
+  addSativa: (sativa) => dispatch(addSativa(sativa)),
+  addIndica: (indica) => dispatch(addIndica(indica)),
+  addHybrid: (hybrid) => dispatch(addHybrid(hybrid))
+
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilterControls);
--- src/Reducers/strainsReducer.js
+++ src/Reducers/strainsReducer.js
@@ -2,11 +2,11 @@
 export const strainsReducer = (state = {}, action) => {
   switch(action.type) {
     case 'ADD_SATIVA':
-      return {...state, sativa: action.results}
+      return {...state, sativa: [action]}
     case 'ADD_INDICA':
-      return {...state, indica: action.results}
+      return {...state, indica: [action]}
     case 'ADD_HYBRID':
-      return {...state, hybrid: action.results}
+      return {...state, hybrid: [action]}
     default:
         return state 
   }
