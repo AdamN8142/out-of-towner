43fedfd4e5a367272d2a7a5578c9ab3e7482f681
Adam Niedzwiecki <adamn8142@gmail.com>
2019-06-01 19:05:00 -0600

    Add id to NavLinks, grab details

--- src/Containers/StrainContainer/StrainContainer.js
+++ src/Containers/StrainContainer/StrainContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom'
 import '../StrainContainer/StrainContainer.scss'
+import { addDescription } from '../../Actions'
 
 class StrainContainer extends Component {
 
@@ -13,6 +14,7 @@ class StrainContainer extends Component {
           return ( 
             <NavLink
             className='navlink'
+            id={`${bud.id}`}
             to={`/${this.props.match.params.strain}/${bud.name}`}
             >{bud.name}</NavLink>
           )
@@ -25,6 +27,7 @@ class StrainContainer extends Component {
           return (
             <NavLink
             className='navlink'
+            id={`${bud.id}`}
             to={`/${this.props.match.params.strain}/${bud.name}`}
             >{bud.name}</NavLink>
           )
@@ -37,6 +40,8 @@ class StrainContainer extends Component {
           return (
             <NavLink
             className='navlink'
+            id={`${bud.id}`}
+            onClick={this.handleClick}
             to={`/${this.props.match.params.strain}/${bud.name}`}
             >{bud.name}</NavLink>
           )
@@ -46,14 +51,25 @@ class StrainContainer extends Component {
     }
   }
   
-
   fetchDetails = async (id) => {
-    const url = `strainapi.evanbusse.com/API_KEY/strains/data/desc/${id}`
-    const response = await fetc(url)
+    const url = `http://strainapi.evanbusse.com/AqtPtuS/strains/data/desc/${id}`
+    const response = await fetch(url)
     const results = await response.json()
     return results
   }
 
+
+
+  handleClick = async (event) => {
+    const id = event.target.id
+    const filter = this.props.filter
+    const details = await this.fetchDetails(id)
+    console.log(details)
+    this.props.addDetails(id, details, filter)
+  }
+
+
+  //I can console log the detils for each one that i click on so i know im getting the right details. just having issued setting it to the store 
 
 
   render(){
@@ -71,7 +87,11 @@ const mapStateToProps = (state) => ({
   filter: state.filter 
   })
 
-export default connect(mapStateToProps)(StrainContainer)
+const mapDispatchToProps = (dispatch) => ({
+  addDetails: (id) => dispatch(addDescription(id))
+}) 
+
+export default connect(mapStateToProps, mapDispatchToProps)(StrainContainer)
 
 
 
--- src/Reducers/strainsReducer.js
+++ src/Reducers/strainsReducer.js
@@ -11,7 +11,7 @@ export const strainsReducer = (state = initialState, action) => {
       return {...state, indica: [action.indica]}
     case 'ADD_HYBRID':
       return {...state, hybrid: [action.hybrid]}
-    case 'ADD-DESCRIPTION':
+    case 'ADD_DESCRIPTION':
       const withDescription = state[action.filter].map((bud)=> {
         if(bud.id === action.id) {
           return {...bud, description: action.description}
@@ -25,3 +25,7 @@ export const strainsReducer = (state = initialState, action) => {
   }
 }
 
+
+
+
+//if i change [action.filter] to a specific strain, it will ren the action but the state of the strain is not updated with a description 
