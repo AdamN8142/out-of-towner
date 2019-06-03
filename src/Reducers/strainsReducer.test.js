bca534ea1ac686762f96ed05bb87eb2f6141941d
Adam Niedzwiecki <adamn8142@gmail.com>
2019-06-03 13:20:02 -0600

    test

---
+++ src/Reducers/strainsReducer.test.js
@@ -0,0 +1,14 @@
+import { strainsReducer } from './strainsReducer'
+import * as actions from '../Actions/index'
+
+describe('strainsReducer', () => {
+ 
+  it('should add the strain of SATIVA', () => {
+    const intialState = {
+      sativa: [],
+      indica: [],
+      hybrid: []
+    }
+    
+  })
+})
