dbf375333ba563d37c9d0bb3a7088502bd819051
Adam Niedzwiecki <adamn8142@gmail.com>
2019-06-03 21:49:32 -0600

    Structure test

--- src/Reducers/strainsReducer.test.js
+++ src/Reducers/strainsReducer.test.js
@@ -16,5 +16,33 @@ describe('strainsReducer', () => {
     const result = strainsReducer(intialState, {})
     expect(result).toEqual(expected)
   })
-  
+  it('should update state of sativa', () => {
+    const intialState = {
+      sativa: [],
+      indica: [],
+      hybrid: []
+    }
+    const action = {
+      type:'ADD_SATIVA', 
+      sativa: 
+        {name:'bobby'}
+    
+  }
+    const expected = [
+      {sativa: {name:'bobby'}
+    }
+  ]
+    const result = strainsReducer([] , action)
+    expect(result).toEqual(expected)
+
+  })
 })
+
+
+// it('should return pokemon', () => {
+//   const action = {type:'ADD_POKEMON', pokemon:[{reptar: 'says hi'}, {nope: 'say hi too'}]}
+//   const expected = [{reptar: 'says hi'}, {nope: 'say hi too'}]
+//   const result = pokemonReducer([], action)
+//   expect(result).toEqual(expected)
+// })
+// })
