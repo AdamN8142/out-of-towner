4ba4caa2008609ef4c0ab0e7f8502b75521e6416
Adam Niedzwiecki <adamn8142@gmail.com>
2019-06-04 22:03:17 -0600

    Test all MSTP/MDTP

---
+++ src/Containers/StrainCard/StrainCard.test.js
@@ -0,0 +1,55 @@
+import { mapStateToProps } from '../StrainCard/StrainCard'
+import * as actions from '../../Actions/index'
+
+describe('mapStateToProps', () => {
+  it('should return a strain prop', () => {
+    const mockState = {
+      strain: {indica: [{a:'1'}, {b:'2'}]},
+      name: 'Adam'
+    }
+    const expected = {
+      strain: {indica: [{a:'1'}, {b:'2'}]},
+    }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  })
+
+  it('should return a description prop', () => {
+    const mockState = {
+      description: 'THE GOOD OL GRATEFULDEAD',
+      name: 'Adam'
+    }
+    const expected = {
+      description: 'THE GOOD OL GRATEFULDEAD',
+    }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  })
+
+  it('should return effects prop', () => {
+    const mockState = {
+      effects: ['i','am','not'],
+      name: 'Adam'
+    }
+    const expected = {
+      effects: ['i','am','not'],
+    }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  })
+
+  it('should return a flavors prop', () => {
+    const mockState = {
+      flavors: ['sweet','chocolate'],
+      name: 'Adam'
+    }
+    const expected = {
+      flavors: ['sweet','chocolate'],
+    }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  })
+})
+
+
+
